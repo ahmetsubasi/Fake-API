ae8fcd65adbf41f519137e7d26c63091f83e3d88
Ahmet Subasi <ahmttsubasii@gmail.com>
2023-08-17 20:24:28 +0300

    fake api

--- App.js
+++ App.js
@@ -1,20 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, Text, View, FlatList, ActivityIndicator, SafeAreaView } from 'react-native';
+import axios from 'axios';
+import UserCard from './src/components/UserCard/UserCard';
+
+
+const URL = 'https://jsonplaceholder.typicode.com/users'
 
 export default function App() {
+
+  const [loading, setLoading] = useState(false)
+  const [userList, setUserList] = useState([])
+
+  async function fetchData(){
+  //setLoading(true)
+  //const response = await axios.get(URL)
+  //setLoading(false)
+  //setUserList(response.data)
+
+  axios.get(URL).then(response => {
+    setLoading(false)
+    setUserList(response.data)
+  })
+  }
+
+
+  const renderUser = ({item}) => <UserCard name={item.name} username={item.username} email={item.email} />
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+  <SafeAreaView>  
+    <View>
+      {
+        loading ? (
+        <ActivityIndicator size="large" color="orange" />
+        )
+        : (      <FlatList
+          data={userList}
+          renderItem={renderUser}
+         /> )
+      } 
+
+      <Button title='Fetch Data' onPress={fetchData}/>
     </View>
+  </SafeAreaView>  
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
